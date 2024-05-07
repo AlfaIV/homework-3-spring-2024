01d59f08e5c202d6b68d5ef12bd372e6530251b1
alfaiv <alfaiv@yandex.ru>
2024-05-07 10:37:03 +0300

    simple example 2

--- Selenium/code/simple_test.py
+++ Selenium/code/simple_test.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import unittest
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-# print(dir(driver))
-elem = driver.find_element(By.NAME ,"q")
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element(By.NAME,"q")
+        elem.send_keys("pycon")
+        assert "No results found." not in driver.page_source
+        elem.send_keys(Keys.RETURN)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
