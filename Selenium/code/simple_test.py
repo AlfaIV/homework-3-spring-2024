a205b90a2f541363352b0f93e4ea850f48739ef0
alfaiv <alfaiv@yandex.ru>
2024-05-09 11:27:19 +0300

    sucess login

--- Selenium/code/simple_test.py
+++ Selenium/code/simple_test.py
@@ -12,34 +12,71 @@ class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def test_search_in_python_org(self):
+    def test_login(self):
         driver = self.driver
         driver.get("http://ads.vk.com")
         login_button = driver.find_element(By.CLASS_NAME,"ButtonCabinet_primary__LCfol")
         login_button.click()
         
-        try:
-            loginForm = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.NAME, "login"))
-            )
-            loginForm.send_keys(login)
-            loginSubmit = driver.find_element(By.CLASS_NAME,"vkuiButton__content")
-            loginSubmit.click()
-        except:
-            print('login falls')
+        # try:
+        #     loginForm = WebDriverWait(driver, 10).until(
+        #         EC.presence_of_element_located((By.NAME, "login"))
+        #     )
+        #     loginForm.send_keys(login)
+        #     loginSubmit = driver.find_element(By.CLASS_NAME,"vkuiButton__content")
+        #     loginSubmit.click()
+        # except:
+        #     print('login falls')
+
+        # try:
+        #     passwordForm = WebDriverWait(driver, 10).until(
+        #         EC.presence_of_element_located((By.NAME, "password"))
+        #     )
+        #     passwordForm.send_keys(password)
+        #     passwordSubmit = driver.find_element(By.CLASS_NAME,"vkuiButton__content")
+        #     passwordSubmit.click()
+        # except:
+        #     print('pws falls')
 
         try:
-            passwordForm = WebDriverWait(driver, 10).until(
+            OAuthBut = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, ".//button[@data-test-id='oAuthService_mail_ru']"))
+            )
+            OAuthBut.click()
+        except:
+            print('oauth falls')
+
+        try:
+            OAuthLogin = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.NAME, "username"))
+            )
+            OAuthLogin.send_keys(login)
+            OAuthSubmitBut = driver.find_element(By.CLASS_NAME, "submit-button-wrap")
+            OAuthSubmitBut.click()
+        except:
+            print('oauth login falls')
+
+        try:
+            OAuthLogin = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.NAME, "password"))
             )
-            passwordForm.send_keys(password)
-            passwordSubmit = driver.find_element(By.CLASS_NAME,"vkuiButton__content")
-            passwordSubmit.click()
+            OAuthLogin.send_keys(password)
+            OAuthSubmitBut = driver.find_element(By.CLASS_NAME, "submit-button-wrap")
+            OAuthSubmitBut.click()
         except:
-            print('pws falls')
+            print('oauth password falls')
+
+        self.assertEqual(driver.current_url, 'https://ads.vk.com/hq/dashboard')
+
+
+    def test_nav_menu(self):
+        driver = self.driver
+        VkIco = driver.find_element(By.CLASS_NAME, "header_left__cv9bp")
+        VkIco.click()
+        self.assertEqual(driver.current_url, 'https://ads.vk.com/hq/dashboard')
 
     def tearDown(self):
-        time.sleep(60)
+        time.sleep(30)
         self.driver.quit()
 
 if __name__ == "__main__":
